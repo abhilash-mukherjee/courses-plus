04ae16ffb8e0adcff48f8ced99ad8e465dea18b8
Abhilash <abhilash301267@gmail.com>
2023-11-10 23:53:33 +0530

    Added endpoints for admin login, signup, add course

--- courses-plus/src/Login.jsx
+++ courses-plus/src/Login.jsx
@@ -8,6 +8,8 @@ import { Typography } from '@mui/material';
 
 
 function Login() {
+    const [email,setEmail] = React.useState("");
+    const [password,setPassword] = React.useState("");
     return (
         <div style={{
             paddingTop: "140px"
@@ -31,29 +33,54 @@ function Login() {
                     }
                 }>
                     <TextField
-                        id="outlined-basic"
                         label="Username"
                         variant="outlined"
                         fullWidth="true"
+                        onChange={(e)=>{
+                            setEmail(e.target.value);
+                        }}
                     />
 
                     <br></br>
                     <br></br>
                     <TextField
-                        id="outlined-basic"
                         label="Password"
                         variant="outlined"
                         fullWidth="true"
-
+                        type= {'password'}
+                        onChange={(e)=>{
+                            setPassword(e.target.value);
+                        }}
                     />
                     <br></br>
                     <br></br>
                     <Button
                         variant="contained"
                         size={'large'}
-                        onClick= {
-                            ()=>{
-                                console.log("login clicked");
+                        onClick={
+                            async () => {
+                                const data = {
+                                    username: email,
+                                    password
+                                };
+                                const response = await fetch('http://localhost:3000/admin/login/',
+                                    {
+                                        method: "POST",
+                                        mode: "cors",
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify(data)
+                                    })
+                                if(response.status ===200){
+                                    response.json().then(body => {
+                                        localStorage.setItem('token',body.token)
+                                    });
+                                }
+                                else{
+                                    console.log("ERROR");
+                                }
+                                    
                             }
                         }
                         >Log In</Button>
